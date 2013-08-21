9d3ac983eb405c60f54aaccc8fbe0c1337e1d3ab
Yan Tao <yantao@letv.com>
2013-08-21 22:24:29 +0800

    Upload code in my package 	modified:   AndroidManifest.xml 	modified:   res/values/strings.xml 	modified:   src/com/teok/android/opengles/my/DrawBasicGeometry.java 	modified:   src/com/teok/android/opengles/my/GLSurfaceViewActivity.java

--- src/com/teok/android/opengles/my/GLSurfaceViewActivity.java
+++ src/com/teok/android/opengles/my/GLSurfaceViewActivity.java
@@ -1,10 +1,71 @@
 package com.teok.android.opengles.my;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
 /**
- * Created with IntelliJ IDEA.
- * <p>Created at 2:47 PM, 8/21/13</p>
- *
- * @author teo
+ * An activity that holds a full screen {@link android.opengl.GLSurfaceView}.
+ * Inheritance should provides its own renderer.
  */
-public class GLSurfaceViewActivity {
+public abstract class GLSurfaceViewActivity extends Activity {
+
+    private GLSurfaceView mGLSurfaceView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(getRenderer());
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * The activity must call the GL surface view's
+         * onResume() on activity onResume().
+         */
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /*
+         * The activity must call the GL surface view's
+         * onPause() on activity onPause().
+         */
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onPause();
+        }
+    }
+
+    protected abstract GLSurfaceView.Renderer getRenderer();
 }
