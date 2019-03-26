ea3272398c5fd2f8dc80748da60c5709a1840611
teoking <yagatong@gmail.com>
2019-03-27 00:18:58 +0800

    Change button style.
    Signed-off-by: teoking <yagatong@gmail.com>

--- app/src/main/java/com/teok/android/common/ListedButtonActivity.kt
+++ app/src/main/java/com/teok/android/common/ListedButtonActivity.kt
@@ -1,6 +1,7 @@
 package com.teok.android.common
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
@@ -10,15 +11,17 @@ import com.teok.android.R
 abstract class ListedButtonActivity : AppCompatActivity() {
 
     private lateinit var mContainer: LinearLayout
+    private lateinit var mInflater: LayoutInflater
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listed_button)
         mContainer = findViewById(R.id.root_container)
+        mInflater = LayoutInflater.from(this)
     }
 
     fun addButton(name: String, onClickListener: View.OnClickListener) {
-        val button = Button(this, null, R.style.NormalButton)
+        val button = mInflater.inflate(R.layout.normal_button, mContainer, false) as Button
         button.text = name
         button.setOnClickListener(onClickListener)
 
